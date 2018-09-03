f94133acc17d58d73e6679d301696f512e06d7f9
OKUGAWA Yuichi <yokugawa@cw04.cc.kek.jp>
2018-09-03 21:16:31 +0900

    TCut modification

--- semi_leptonic/efficiency.C
+++ semi_leptonic/efficiency.C
@@ -14,15 +14,14 @@ void efficiency()
 		TTree * GenTree = (TTree*) file->Get( "GenTree" ) ;
 		TTree * Summary = (TTree*) file->Get( "Summary" );
 
-		int forward = GenTree->Draw("qMCcostheta","qMCcostheta > 0 && qMCcostheta > -2 ");
+		int forward  = GenTree->Draw("qMCcostheta","qMCcostheta > 0 && qMCcostheta > -2 ");
 		int backward = GenTree->Draw("qMCcostheta","qMCcostheta < 0 && qMCcostheta > -2");
 
-		
 		///////////////
 		// BASE CUTS //
 		///////////////
 
-		string cuts = "&& hadMass > 180 && hadMass < 420 && Top1mass < 270 && W1mass < 250 && Top1mass > 120 && W1mass > 50 && ((Top1gamma + Top2gamma) > 2.4  && Top2gamma < 2 ) && (Top1btag > 0.0 || Top2btag > 0.0)  && (Top1bmomentum > 15 && Top2bmomentum > 15) && methodTaken > 0";
+		//string cuts = "&& hadMass > 180 && hadMass < 420 && Top1mass < 270 && W1mass < 250 && Top1mass > 120 && W1mass > 50 && ((Top1gamma + Top2gamma) > 2.4  && Top2gamma < 2 ) && (Top1btag > 0.8 || Top2btag > 0.3)  && (Top1bmomentum > 15 && Top2bmomentum > 15) && methodTaken > 0";
 
 		// **** Full cuts ****
 		//string cuts = "&& hadMass > 180 && hadMass < 420 && Top1mass < 270 && W1mass < 250 && Top1mass > 120 && W1mass > 50 && ((Top1gamma + Top2gamma) > 2.4  && Top2gamma < 2 ) && ((methodTaken == 1 && Top1btag > 0.8 && Top2btag > 0.8 && Top1bmomentum > 35 && Top2bmomentum > 35) || methodTaken > 1 )";
@@ -64,11 +63,30 @@ void efficiency()
 		TCut hadM = "hadMass > 180 && hadMass < 420";
 		TCut rcTW = "Top1mass < 270 && W1mass < 250 && Top1mass > 120 && W1mass > 50";
 
+		TCut pcut = "Top1bmomentum > 15 && Top2bmomentum > 15";
+		TCut gcut = "(Top1gamma + Top2gamma) > 2.4  && Top2gamma < 2";
 
-		string fcuts = "qCostheta > 0" + cuts;
-		string bcuts = "qCostheta < 0 && qCostheta > -1.0 " + cuts;
-		int recoforward = normaltree->Draw("qCostheta", fcuts.c_str());
-		int recobackward = normaltree->Draw("qCostheta", bcuts.c_str());
+		TCut method = "methodTaken > 0";
+
+		TCut cuts = thru && hadM && rcTW && pcut && gcut && method;
+
+		int afterbcut    = normaltree->GetEntries();
+		int afterthrucut = normaltree->GetEntries( thru );
+		int afterhadMcut = normaltree->GetEntries( thru && hadM );
+		int afterrcTWcut = normaltree->GetEntries( thru && hadM && rcTW );
+
+		cout << "after thrust cut             = " << afterthrucut << " (" << (float)(afterthrucut)/(float)(nevt) *100 << "%)" << endl;
+		cout << "after hadronic mass cut      = " << afterhadMcut << " (" << (float)(afterhadMcut)/(float)(nevt) *100 << "%)" << endl;
+		cout << "after reco T & W mass cut    = " << afterrcTWcut << " (" << (float)(afterrcTWcut)/(float)(nevt) *100 << "%)" << endl;
+		
+		TCut fcoscut = "qCostheta > 0";
+		TCut bcoscut = "qCostheta < 0 && qCostheta > -1.0";
+	
+		TCut fcuts = fcoscut && cuts;
+		TCut bcuts = bcoscut && cuts;
+
+		int recoforward = normaltree->GetEntries(fcuts);
+		int recobackward = normaltree->GetEntries(bcuts);
 		
 		float afbgen = (float)(forward - backward) / (float) (forward + backward);
 		float afbreco = (float)(recoforward - recobackward) / (float) (recoforward + recobackward);
