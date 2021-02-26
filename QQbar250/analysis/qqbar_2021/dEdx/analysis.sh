2d296049f3781410025f8ae64f40cb8a064e5364
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-02-26 23:37:58 +0900

    measure time

--- QQbar250/analysis/qqbar_2021/dEdx/analysis.sh
+++ QQbar250/analysis/qqbar_2021/dEdx/analysis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+start_time=`date +%s`
+
 filein=/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/proc_2021/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.MERGE50.root
 
 root -l -q analysis.cc\(\"$filein\",\"test\",true,true\) &
@@ -8,6 +10,8 @@ root -l -q analysis.cc\(\"$filein\",\"test\",true,false\)
 root -l -q analysis.cc\(\"$filein\",\"test\",false,true\) &
 root -l -q analysis.cc\(\"$filein\",\"test\",false,false\) 
 
+end_time=`date +%s`
 
+time=$((end_time - start_time))
 
-
+echo $time
