c086ba454dc48d686cf747c1cc0e9d73b0225150
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-02-26 23:35:19 +0900

    finer dEdx binning / add proper file path

--- QQbar250/analysis/qqbar_2021/dEdx/analysis.sh
+++ QQbar250/analysis/qqbar_2021/dEdx/analysis.sh
@@ -1,8 +1,12 @@
-root -l -q analysis.cc\(\"/mnt/HardDrive/cernbox_hd/QQbar/processor_output_2021/test.root\",\"test\",true,true\) &
-root -l -q analysis.cc\(\"/mnt/HardDrive/cernbox_hd/QQbar/processor_output_2021/test.root\",\"test\",true,false\)
+#!/bin/bash
 
-root -l -q analysis.cc\(\"/mnt/HardDrive/cernbox_hd/QQbar/processor_output_2021/test.root\",\"test\",false,true\) &
-root -l -q analysis.cc\(\"/mnt/HardDrive/cernbox_hd/QQbar/processor_output_2021/test.root\",\"test\",false,false\) 
+filein=/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/proc_2021/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.MERGE50.root
+
+root -l -q analysis.cc\(\"$filein\",\"test\",true,true\) &
+root -l -q analysis.cc\(\"$filein\",\"test\",true,false\)
+
+root -l -q analysis.cc\(\"$filein\",\"test\",false,true\) &
+root -l -q analysis.cc\(\"$filein\",\"test\",false,false\) 
 
 
 
