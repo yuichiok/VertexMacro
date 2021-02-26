3684a239eadc96be5e2fb1f19c414317906bb014
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-02-26 16:23:52 +0900

    minor change

--- QQbar250/analysis/kaon_analysis/analysis.cc
+++ QQbar250/analysis/kaon_analysis/analysis.cc
@@ -8,8 +8,7 @@ int analysis(){
 //  TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15161/000/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.MERGED.root";
 //  TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/000/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.MERGE50.root";
 
-  // TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/adrian_sample/bbbar15162_radret_250GeV_mc2020_eL_ValenciaVertexBeam_R1.17_short2.root";
-  TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/proc_2021/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.MERGE50.root";
+  TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/adrian_sample/bbbar15162_radret_250GeV_mc2020_eL_ValenciaVertexBeam_R1.17_short2.root";
 
   TString output = "bbbar_Kgamma35_15162_cuts12_250GeV_eL_btag1_0.8_btag2_0.8_nbins40";
 
