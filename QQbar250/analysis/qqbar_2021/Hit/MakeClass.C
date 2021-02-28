710720872cf4b4908267e14bba07f8b9cda2c21b
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-02-28 17:57:35 +0900

    TPC hit analysis

---
+++ QQbar250/analysis/qqbar_2021/Hit/MakeClass.C
@@ -0,0 +1,23 @@
+#include "TROOT.h"
+#include "TFile.h"
+
+int MakeClass(){
+
+  TString filename = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/proc_2021/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.MERGE50.root";
+  TFile *file = new TFile(filename);
+
+  TString treename = "Stats";
+  TTree* tree = (TTree*)file->Get( treename ) ;
+
+  file->ls();
+
+  tree->MakeClass("Hit");
+
+  std::cout << "processed..." << std::endl;
+
+  gSystem -> Exit(0);
+
+  return 0;
+
+
+}
