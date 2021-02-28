f2d13541462f1b8e5d1bf6c2cbf66345f2007f9f
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-03-01 01:00:21 +0900

    add hit histograms

--- QQbar250/analysis/qqbar_2021/Hit/Hit.cc
+++ QQbar250/analysis/qqbar_2021/Hit/Hit.cc
@@ -7,7 +7,12 @@
 void Hit::AnalyzeHit(int n_entries=-1, float Kvcut=35, TString output="test")
 {
 
-	//TH2F* h_HitCos = 
+	TH2F* h_HitCos_all  = new TH2F("h_HitCos_all",";|cos#theta|; # of Track Hits",100,0,1,230,0,230);
+	TH2F* h_HitCos_k 	= new TH2F("h_HitCos_k",";|cos#theta|; # of Track Hits",100,0,1,230,0,230);
+
+	TFile *MyFile = new TFile(TString::Format("DQ_250GeV_%s.root",output.Data()),"RECREATE");
+	MyFile->cd();
+
 
 	if (fChain == 0) return;
 
@@ -27,17 +32,24 @@ void Hit::AnalyzeHit(int n_entries=-1, float Kvcut=35, TString output="test")
 		for(int i=0; i<pfo_n; i++) {
 
 			VecOP pfoVec(pfo_px[i],pfo_py[i],pfo_pz[i]);
+			float abscos = abs( pfoVec.GetCostheta() );
+
+			h_HitCos_all->Fill(abscos, pfo_tpc_hits[i]);
+			if(fabs(pfo_pdgcheat[i])==321) h_HitCos_k->Fill(abscos, pfo_tpc_hits[i]);
 		
 			// std::cout << "pfo" << i << std::endl;
 			//std::vector< float > angles = CalculateAngles(pfo_px[i],pfo_py[i],pfo_pz[i]);
 
-
 		}
 
 		// trial
-		if(jentry==0) break;
+		// if(jentry==1000) break;
 
-	}
+	} // end of event loop
+
+	h_HitCos_all->Write();
+	h_HitCos_k->Write();
+
 }
 
 bool Hit::PreSelection(int type=0,float Kvcut=25) {
