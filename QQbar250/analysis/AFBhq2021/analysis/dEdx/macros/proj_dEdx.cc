7e700639d05c5ea82eba300a11bb48b31f87d056
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-09-29 13:20:49 +0900

    individual projection

--- QQbar250/analysis/AFBhq2021/analysis/dEdx/macros/proj_dEdx.cc
+++ QQbar250/analysis/AFBhq2021/analysis/dEdx/macros/proj_dEdx.cc
@@ -69,6 +69,8 @@ void drawTGEs(TGraphErrors* TGEs[]){
   for (int i = 0; i < NPAR; ++i){
 
     TGEs[i]->SetLineColor(colors[i]);
+    TGEs[i]->SetMarkerColor(colors[i]);
+    TGEs[i]->SetMarkerSize(1.0);
     
     if(i==0){
       TGEs[i]->Draw("AP");
@@ -106,7 +108,7 @@ void proj_dEdx() {
 
   //////////// Projection Momentum dE/dx ////////////
 
-  TCanvas* cs[5];
+  TCanvas* cs_proj[5];
   float rLow[5]  = {1.0,2.0,5.0,10.0,30.0};
   float rHigh[5] = {1.5,2.5,6.0,11.5,40.0};
   
@@ -114,7 +116,7 @@ void proj_dEdx() {
   {
     TAxis* xaxis = dEdx_truths[i]->GetXaxis();
 
-    cs[i] = new TCanvas(TString::Format("mom_%.1f_%.1f",rLow[i],rHigh[i]),TString::Format("mom_%.1f_%.1f",rLow[i],rHigh[i]),500,500);
+    cs_proj[i] = new TCanvas(TString::Format("mom_%.1f_%.1f",rLow[i],rHigh[i]),TString::Format("mom_%.1f_%.1f",rLow[i],rHigh[i]),500,500);
     TH1D* projs[5];
 
     for (int j = 0; j < NPAR; ++j)
@@ -122,7 +124,7 @@ void proj_dEdx() {
       projs[j] = dEdx_truths[j]->ProjectionY(TString::Format("proj_%s",particles[j].Data()),xaxis->FindBin(rLow[i]),xaxis->FindBin(rHigh[i]));
     }
 
-    drawTH1D(cs[i],projs);
+    drawTH1D(cs_proj[i],projs);
 
   }
 
@@ -130,11 +132,6 @@ void proj_dEdx() {
   //////////// Projection Individual dE/dx ////////////
 
   float momentum_min=0.95;
-
-  // //kaon
-
-  TCanvas* c_ind_proj = new TCanvas("c_ind_proj","c_ind_proj",500,500);
-  c_ind_proj->cd();
 
   TH1D *ind_projs[NPAR];
   TGraphErrors *GEprojs[NPAR];
@@ -145,37 +142,56 @@ void proj_dEdx() {
   {
     float x[200], y[200], ey[200], ey2[200];
     float ex[200];
-    int n_particle=0;
+    int npt=0;
 
     for(int j=0; j<dEdx_truths[i]->GetNbinsX(); j++) {
 
       ind_projs[i] =dEdx_truths[i]->ProjectionY(TString::Format("ind_proj_%s",particles[i].Data()),j,j+1);
 
-      x[n_particle]=dEdx_truths[i]->GetXaxis()->GetBinCenter(j+1);
-      y[n_particle]=ind_projs[i]->GetMean();
-      ey[n_particle]=ind_projs[i]->GetRMS();
-      ey2[n_particle]=ind_projs[i]->GetMean()/sqrt(ind_projs[i]->GetEntries());
-      ex[n_particle]=0;//dEdx_truths[0]->GetXaxis()->GetBinWidth(i+1)/2.;
-      n_particle++;
+      x[npt]=dEdx_truths[i]->GetXaxis()->GetBinCenter(j+1);
+      y[npt]=ind_projs[i]->GetMean();
+      ey[npt]=ind_projs[i]->GetRMS();
+      ey2[npt]=ind_projs[i]->GetMean()/sqrt(ind_projs[i]->GetEntries());
+      ex[npt]=0;//dEdx_truths[0]->GetXaxis()->GetBinWidth(i+1)/2.;
+      npt++;
 
     }
 
-    GEprojs[i]  = new TGraphErrors(n_particle,x,y,ex,ey);
-    GEproj2s[i] = new TGraphErrors(n_particle,x,y,ex,ey2);
+    GEprojs[i]  = new TGraphErrors(npt,x,y,ex,ey);
+    GEprojs[i]->SetTitle(TString::Format("%s;p [GeV];#frac{dE}{dx} [MeV]",particles[i].Data()));
+    GEproj2s[i] = new TGraphErrors(npt,x,y,ex,ey2);
+    GEproj2s[i]->SetTitle(TString::Format("%s_2;p [GeV];#frac{dE}{dx} [MeV]",particles[i].Data()));
 
   }
 
+  TCanvas* c_ind_proj = new TCanvas("c_ind_proj","c_ind_proj",500,500);
+  c_ind_proj->cd();
+  c_ind_proj->SetLogx();
+
   drawTGEs(GEprojs);
 
+  TCanvas* c_ind_proj2 = new TCanvas("c_ind_proj2","c_ind_proj2",500,500);
+  c_ind_proj2->cd();
+  c_ind_proj2->SetLogx();
+
+  drawTGEs(GEproj2s);
 
   //////////// Save Plots ////////////
 
-  // TFile *MyFile = new TFile("proj_dEdx.root","RECREATE");
-  // MyFile->cd();
-  // for (int i = 0; i < 5; ++i)
-  // {
-  //   cs[i]->Write();
-  // }
+  TFile *MyFile = new TFile("proj_dEdx.root","RECREATE");
+  MyFile->cd();
+  for (int i = 0; i < 5; ++i){ 
+    cs_proj[i]->Write(); 
+  }
+
+  for (int i = 0; i < NPAR; ++i)
+  {
+    GEprojs[i]->Write();
+    GEproj2s[i]->Write();
+  }
+
+  c_ind_proj->Write();
+  c_ind_proj2->Write();
 
 
  std::cout << "DONE" << std::endl;
