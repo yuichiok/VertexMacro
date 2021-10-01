6bb90d57088a2d74c3275b6e39be8cd490a34278
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-10-01 15:32:34 +0900

    dEdx distance plot with difference momentum cuts

--- QQbar250/analysis/AFBhq2021/analysis/dEdx/analysis.cc
+++ QQbar250/analysis/AFBhq2021/analysis/dEdx/analysis.cc
@@ -8,7 +8,7 @@
 int analysis(){
 
   TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/dEdx_corr/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.400.root";
-  TString output = "/home/ilc/yokugawa/macros/QQbar250/analysis/AFBhq2021/analysis/dEdx/rootfiles/dEdx_test.root";
+  TString output = "/home/ilc/yokugawa/macros/QQbar250/analysis/AFBhq2021/analysis/dEdx/rootfiles/dEdx_pcut_2.root";
 
   cout<< " "<<endl;
   cout<< file << endl;
@@ -17,7 +17,7 @@ int analysis(){
   observable ss3(file);
   
   // ss3.dEdx(-1,TString::Format("%s_cut_%i",output.Data(),cut),true,ignoreoverlay,float(cut),quark); 
-  ss3.dEdx(-1,output,false,true,0,3); 
+  ss3.dEdx(-1,output,false,true,2.0,3); 
 
   //  ss3.dEdx(-1,output,false,ignoreoverlay);
   
