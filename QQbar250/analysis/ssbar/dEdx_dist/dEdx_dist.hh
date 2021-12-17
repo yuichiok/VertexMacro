5b71e84a5868cb55124cd9d367bfeb6d837d6d4d
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2021-12-18 02:33:48 +0900

    declaration of kaon counter variable 修正

--- QQbar250/analysis/ssbar/dEdx_dist/dEdx_dist.hh
+++ QQbar250/analysis/ssbar/dEdx_dist/dEdx_dist.hh
@@ -388,9 +388,7 @@ private:
    int n_cos_nonconsis=0;
 
    // int n_gen_kaon     = 0;
-   int n_reco_kaon_all     = 0;
-   int n_reco_kaon_jet[2]  = {0};
-
+   
    int nevents_kaon_match = 0;
 
    int n_kk   = 0;
