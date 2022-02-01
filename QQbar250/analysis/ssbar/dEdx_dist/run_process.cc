23934af2af042510406fbce27ad68cc6402da4eb
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2022-02-01 14:31:01 +0100

    set path for lxplus

--- QQbar250/analysis/ssbar/dEdx_dist/run_process.cc
+++ QQbar250/analysis/ssbar/dEdx_dist/run_process.cc
@@ -25,7 +25,8 @@ int run_process(TString output="default"){
 
   // TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/proc_2021/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.root";
   // TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/dEdx_corr/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.root";
-  TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/dEdx_corr/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.400.root";
+  // TString file = "/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/dEdx_corr/QQbarProcessor_out/root_merge/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.400.root";
+  TString file = "/afs/cern.ch/work/y/yuokugaw/DataSets/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.15162.PID.400.root";
   
   dEdx_dist dEdx_dist(file);
   dEdx_dist.Analyze_dEdxdist(-1,minp,output);
