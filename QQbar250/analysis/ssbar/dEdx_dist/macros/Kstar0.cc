9f1c53247ecc2af827f6b4ce494e406aefaf25b4
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2022-07-15 15:25:15 +0200

    plot k star masses

--- QQbar250/analysis/ssbar/dEdx_dist/macros/Kstar0.cc
+++ QQbar250/analysis/ssbar/dEdx_dist/macros/Kstar0.cc
@@ -5,13 +5,17 @@
 
 void Kstar0(){
 
-    TFile *f = TFile::Open("../downloads/DQ_250GeV_ss.minp20maxp60.hit210.offset.dEdxMin.OppKMult.KPicross.K_mass_correct.mergeFULL.root","UPDATE");
+    gStyle->SetOptStat(0);
+
+    TFile *f = TFile::Open("../rootfiles/double_tag/DQ_250GeV_ss.minp20maxp60.hit210.offset.dEdxMin.OppKMult.KPicross.K_mass_correct.mergeFULL.root","READ");
     if (f == 0) {
         std::cout << "Error: File not found." << std::endl;
         return;
     }
 
-    TH1F * hs[11];
+    const int hsize = 11;
+    TH1F * hs[hsize];
+    int colors[hsize] = {1,633,810,418,434,601,617,907,804,800,875};
 
     hs[0] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass");
     hs[1] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_cheat313");
@@ -25,14 +29,42 @@ void Kstar0(){
     hs[9] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_cheat92");
     hs[10] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_cheat_other");
 
-    hs[0]->SetLineColor(kBlack);
-    hs
+    
+    TLegend *leg0 = new TLegend(0.65,0.85,0.9,0.5,"","brNDC");
+    TString labels[hsize] = {"All","K*_{0}(892)^{0}","K*(other)^{0}","K*_{2}(1430)^{0}","K*(892)^{#pm}","#phi(1020)","f'_{2}(1525)","#rho(770)^{0}","#rho(770)^{#pm}","MC hadron","others"};
+    leg0->SetFillStyle(0);
+    leg0->SetBorderSize(0);
+    leg0->SetTextSize(0.025);
+    for (int ih = 0; ih < hsize; ih++) leg0->AddEntry(hs[ih],labels[ih],"lf");
 
+    TLegend *leg1 = new TLegend(0.65,0.85,0.9,0.5,"","brNDC");
+    leg1->SetFillStyle(0);
+    leg1->SetBorderSize(0);
+    leg1->SetTextSize(0.025);
+    for (int ih = 0; ih < 10; ih++) leg1->AddEntry(hs[ih],labels[ih],"lf");
+
+
+    TCanvas *c0 = new TCanvas("c0","c0",700,700);
     THStack *h_KstarStack  = new THStack("h_KstarStack","h_KstarStack");
+    hs[0]->SetLineWidth(3);
+    for (int ih = 0; ih < hsize; ih++)
+    {
+        hs[ih]->SetLineColor(colors[ih]);
+        hs[ih]->SetFillColor(colors[ih]);
+        hs[ih]->SetFillStyle(3004);
+        hs[ih]->Rebin(4);
+        hs[ih]->GetXaxis()->SetRangeUser(0.7,2);
+        if(0<ih && ih<10) h_KstarStack->Add(hs[ih]);
+    }
+    hs[0]->Draw("h");
+    h_KstarStack->Draw("nostack same");
+    hs[10]->Draw("hsame");
+    leg0->Draw("same");
 
-    TCanvas *c = new TCanvas("c","c",700,700);
 
-
-
+    TCanvas *c1 = new TCanvas("c1","c1",700,700);
+    hs[0]->Draw("h");
+    h_KstarStack->Draw("same");
+    leg1->Draw("same");
 
 }
