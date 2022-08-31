a0040ab8266c6896935ad6d699ed2c505ee7aa1c
yuichiok <yuichi.okugawa@cern.ch>
2022-09-01 01:29:26 +0200

    chnage settings batch

--- QQbar250/analysis/ssbar/dEdx_dist/runDir.sh
+++ QQbar250/analysis/ssbar/dEdx_dist/runDir.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-#SBATCH --time=1
 
 # data_input=/group/ilc/users/yokugawa/QQbar250/l5/eLpR/15162/dEdx_corr/QQbarProcessor_out/
 data_input=/sps/ilc/yokugawa/QQbar250/l5/eLpR/00015162/QQbarProcessor_out/
 
 nfirst=1
 nlast=-1
-nrun=200
+nrun=10
 
 flist=list/15162.list
 
@@ -34,5 +33,5 @@ for seq in $seqlist; do
     echo ${id} $nrun0 $nrun1 $nrun $njobs
 
     log=./sublog/`printf %03d $seq`.log
-    sbatch -N1 --partition=hpc ./runROOT ${flist} $nrun0 $nrun1 -o $log
+    sbatch -N1 -t 10 -o slurm_out/slurm-%j.out --partition=hpc ./runROOT ${flist} $nrun0 $nrun1 -o $log
 done
