4567c3589457d25f44a70a8de1a75aca333f21b5
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2022-09-01 18:59:56 +0200

    Inv Mass Pi corrected for phi

--- QQbar250/analysis/ssbar/dEdx_dist/macros/Kstar0.cc
+++ QQbar250/analysis/ssbar/dEdx_dist/macros/Kstar0.cc
@@ -3,11 +3,18 @@
 #include <iostream>
 #include <stdlib.h>
 
+void format_hist(TH1F* h){
+    h->SetFillStyle(3004);
+    h->Rebin(4);
+    h->GetXaxis()->SetRangeUser(0.7,2);
+}
+
 void Kstar0(){
 
     gStyle->SetOptStat(0);
 
-    TFile *f = TFile::Open("../rootfiles/double_tag/DQ_250GeV_ss.minp20maxp60.hit210.offset.dEdxMin.OppKMult.KPicross.K_cos.AllEvents.root","READ");
+    // TFile *f = TFile::Open("../rootfiles/double_tag/DQ_250GeV_ss.minp20maxp60.hit210.offset.dEdxMin.OppKMult.KPicross.K_cos.AllEvents.root","READ");
+    TFile *f = TFile::Open("../rootfiles/double_tag/DQ_250GeV_ss.minp20maxp60.hit210.offset.dEdxMin.OppKMult.KPicross.K_cos.mergeFULL.root","READ");
     if (f == 0) {
         std::cout << "Error: File not found." << std::endl;
         return;
@@ -29,6 +36,17 @@ void Kstar0(){
     hs[9] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_cheat92");
     hs[10] = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_cheat_other");
 
+    TH1F * h_phi_corr_invM = (TH1F*)f->Get("h_pfo_LeadPi_K_mass_reco333");
+    h_phi_corr_invM->SetLineColor(kRed+2);
+    h_phi_corr_invM->SetFillColor(kRed+2);
+    format_hist(h_phi_corr_invM);
+    
+    TH1F * h_pfo_LeadPi_K_corr_phi_mass = (TH1F*)f->Get("h_pfo_LeadPi_K_corr_phi_mass");
+    h_pfo_LeadPi_K_corr_phi_mass->SetLineColor(kRed);
+    h_pfo_LeadPi_K_corr_phi_mass->SetFillColor(kRed);
+    format_hist(h_pfo_LeadPi_K_corr_phi_mass);
+    h_pfo_LeadPi_K_corr_phi_mass->SetLineWidth(3);
+    h_pfo_LeadPi_K_corr_phi_mass->SetLineStyle(2);
     
     TLegend *leg0 = new TLegend(0.65,0.85,0.9,0.5,"","brNDC");
     TString labels[hsize] = {"All","K*_{0}(892)^{0}","K*(other)^{0}","K*_{2}(1430)^{0}","K*(892)^{#pm}","#phi(1020)","f'_{2}(1525)","#rho(770)^{0}","#rho(770)^{#pm}","MC hadron","others"};
@@ -51,9 +69,7 @@ void Kstar0(){
     {
         hs[ih]->SetLineColor(colors[ih]);
         hs[ih]->SetFillColor(colors[ih]);
-        hs[ih]->SetFillStyle(3004);
-        hs[ih]->Rebin(4);
-        hs[ih]->GetXaxis()->SetRangeUser(0.7,2);
+        format_hist(hs[ih]);
         if(0<ih && ih<10) h_KstarStack->Add(hs[ih]);
     }
     hs[0]->Draw("h");
@@ -66,5 +82,13 @@ void Kstar0(){
     hs[0]->Draw("h");
     h_KstarStack->Draw("same");
     leg1->Draw("same");
+
+    TCanvas *c2 = new TCanvas("c2","c2",700,700);
+    hs[0]->GetYaxis()->SetRangeUser(0,540);
+    hs[0]->Draw("h");
+    h_pfo_LeadPi_K_corr_phi_mass->Draw("hsame");
+    // h_KstarStack->Draw("nostack same");
+    h_phi_corr_invM->Draw("hsame");
+    leg0->Draw("same");
 
 }
